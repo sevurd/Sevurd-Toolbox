e910fcd3877f8bb713052a0d3c40eff026b0bf3e
Sevurd <101691490+sevurd@users.noreply.github.com>
2022-03-22 00:28:53 +0530

    Update Sevurd-Toolbox.ps1

--- Sevurd-Toolbox.ps1
+++ Sevurd-Toolbox.ps1
@@ -1059,7 +1059,7 @@ $essentialtweaks.Add_Click({
     powercfg -delete 381b4222-f694-41f0-9685-ff5bb260df2e
     powercfg -delete 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c 
     powercfg -delete a1841308-3541-4fab-bc81-f71556f20b4a
-    Write-Host "Inport Nvidia Settings"
+    Write-Host "Import Nvidia Settings"
     _nvidiaProfileInspector.exe "C:\Windows\_NvidiaBaseProfile.nip"
     Write-Host "Installing Timer Resolution Service"
     _SetTimerResolutionService.exe -install
